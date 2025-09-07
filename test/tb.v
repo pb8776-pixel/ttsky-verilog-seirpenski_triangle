87b6c7c767008ee12885306e89f2e9d6bff603e6
pb8776-pixel <pb8776@srmist.edu.in>
2025-09-07 18:01:08 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  seirpenski_triangle (
+  tt_um_seirpenski_triangle user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
